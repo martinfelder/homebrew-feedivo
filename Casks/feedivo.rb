d1c99ecc9dc9ae8f700ce491862fd68bf70cf365
Martin Felder <martin.felder@outlook.com>
2026-08-02 19:03:28 +0200

    feat: Feedivo 1.0 (24)

--- Casks/feedivo.rb
+++ Casks/feedivo.rb
@@ -1,6 +1,6 @@
 cask "feedivo" do
-  version "1.0,23"
-  sha256 "ae3527e593fea9fb4311c1c6245cc53375a0654da438d6dddc69713e32875ee6"
+  version "1.0,24"
+  sha256 "ddfbc6502edbfbdd3ebfc6786e5ef2d1eeec7930abf32c73381edef49ce4b804"
 
   url "https://github.com/martinfelder/feedivo-mac/releases/download/v#{version.csv[0]}-#{version.csv[1]}/Feedivo-v#{version.csv[0]}-#{version.csv[1]}.zip"
   name "Feedivo"
